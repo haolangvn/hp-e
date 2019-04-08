ce808393ece034700881b9f399e633b21f331f23
= <=>
2019-04-08 16:32:17 +0700

    init

---
+++ src/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace hp\ecommerce;
+
+/**
+ * Description of Test
+ *
+ * @author HAO
+ */
+class Test {
+
+    public static function test() {
+        echo '2222222222222222';
+    }
+
+}
---
+++ src/helpers/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace hp\ecommerce\helpers;
+
+/**
+ * Description of Test
+ *
+ * @author HAO
+ */
+class Test {
+
+    public static function test() {
+        echo 'Test';
+    }
+
+}
